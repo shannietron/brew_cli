b8cb124b03bb16cd68d19e5c5f2c08f6af6ab2e4
Shanmugam <git@shan.mp>
2016-08-23 23:58:01 +0800

    playing with hyper to perform get requests

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+extern crate hyper;
+use hyper::client::Client;
+use hyper::client::Response;
+use std::io::Read;
+
 fn main() {
-    println!("Hello, world!");
+	let client = Client::new();
+	let mut res: Response = client.get("https://api.thingspeak.com/channels/98643/feed/last.json").send().unwrap();
+			assert_eq!(res.status, hyper::Ok);
+	let mut buffer = String::new();
+	res.read_to_string(&mut buffer);
+
+	println!("{}",res.url);
+	println!("{}",buffer);
 }
